6b3e8d6e7b4bfd6e86e5f4477c7ca9ac653555df
Vinícius Feitosa da Silva <vinicius.feitosa@cultura.gov.br>
2016-09-13 17:34:35 -0300

    [update] Ajuste para que seja possível enviar parametros para o arquivo jsp para o qual será redirecionado após o login

--- src/main/java/br/com/xti/ouvidoria/security/LoginServlet.java
+++ src/main/java/br/com/xti/ouvidoria/security/LoginServlet.java
@@ -2,8 +2,11 @@ package br.com.xti.ouvidoria.security;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
+import javax.jws.WebMethod;
+import javax.jws.WebService;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
@@ -12,9 +15,16 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.hibernate.validator.constraints.URL;
+
+import com.google.api.client.auth.oauth2.AuthorizationCodeFlow;
+import com.google.api.client.auth.oauth2.CredentialRefreshListener;
 
 import br.com.xti.ouvidoria.helper.ValidacaoHelper;
 import br.com.xti.ouvidoria.model.TbManifestacao;
+import br.com.xti.ouvidoria.security.oauth.Authorization;
 
 @WebServlet(description = "Servlet responsável por efetuar o login do usuário", urlPatterns = { "/login" })
 public class LoginServlet extends HttpServlet {
@@ -36,10 +46,12 @@ public class LoginServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		ServletContext context = getServletContext();
 	    String versao = context.getInitParameter("versao");
-	    
+
 		request.setAttribute("versao", versao);
 		sendRedirect(request, response);
 	}
+	
+	
 	
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -80,37 +92,41 @@ public class LoginServlet extends HttpServlet {
 		}
 	}
 	
-	private void sendRedirect(HttpServletRequest request, HttpServletResponse response) throws IOException {
+	private void sendRedirect(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 		String pageToSend = request.getContextPath();
-		
-		if(request.getUserPrincipal() != null) {
-			LoginTypeEnum loginType = (LoginTypeEnum) request.getAttribute("loginType");
+		try {
 			
-			if(loginType == null) {
-				pageToSend = pageToSend.concat(LOGGED_INDEX_PAGE);
-			} else {
-				switch (loginType) {
-					case USER:
-						pageToSend = pageToSend.concat(LOGGED_INDEX_PAGE);
-						break;
-					case MANIFESTATION: {
-						TbManifestacao manifestation = (TbManifestacao) request.getAttribute("manifestation");
-						pageToSend = pageToSend.concat(String.format(
-								MANIFESTATION_DETAIL_PAGE,
-								manifestation.getNrManifestacao(),
-								manifestation.getIdManifestacao()));
-						break;
+			if(request.getUserPrincipal() != null) {
+				LoginTypeEnum loginType = (LoginTypeEnum) request.getAttribute("loginType");
+				
+				if(loginType == null) {
+					pageToSend = LOGGED_INDEX_PAGE;
+				} else {
+					switch (loginType) {
+						case USER:
+							pageToSend = LOGGED_INDEX_PAGE;
+							break;
+						case MANIFESTATION: {
+							TbManifestacao manifestation = (TbManifestacao) request.getAttribute("manifestation");
+							pageToSend = String.format(
+									MANIFESTATION_DETAIL_PAGE,
+									manifestation.getNrManifestacao(),
+									manifestation.getIdManifestacao());
+							break;
+						}
+						case NEW_PASSWORD:
+							pageToSend = "/novasenha.xhtml";
+							break;
 					}
-					case NEW_PASSWORD:
-						pageToSend = pageToSend.concat("/novasenha.xhtml");
-						break;
 				}
+			} else {
+				pageToSend = "/login.jsp";
 			}
-		} else {
-			pageToSend = pageToSend.concat("/login.jsp");
+			
+			RequestDispatcher dispatcher = request.getRequestDispatcher(pageToSend);
+			dispatcher.forward(request, response);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
 		}
-		
-		response.sendRedirect(pageToSend);
 	}
-	
 }
