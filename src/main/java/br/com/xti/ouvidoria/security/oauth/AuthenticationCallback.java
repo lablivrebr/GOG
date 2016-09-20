d95294b787d25f28ae764beb92ee44a6f8cae1dc
Vinícius Feitosa da Silva <vinicius.feitosa@cultura.gov.br>
2016-09-20 15:39:55 -0300

    [ update ]  Definição  da propriedade privada usuarioDAO com uma anotação @EJB para que seja preenchido através de Injeção de dependência. \ [ update ] Adição de bloco inteiro dentro do 'try'.

--- src/main/java/br/com/xti/ouvidoria/security/oauth/AuthenticationCallback.java
+++ src/main/java/br/com/xti/ouvidoria/security/oauth/AuthenticationCallback.java
@@ -1,57 +1,48 @@
 package br.com.xti.ouvidoria.security.oauth;
 
 import java.io.File;
-
 import java.io.IOException;
-import java.security.SecureRandom;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
+import javax.ejb.EJB;
+import javax.inject.Inject;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.primefaces.json.JSONObject;
 
 import com.google.api.client.auth.oauth2.AuthorizationCodeFlow;
 import com.google.api.client.auth.oauth2.AuthorizationCodeResponseUrl;
-import com.google.api.client.auth.oauth2.AuthorizationCodeTokenRequest;
 import com.google.api.client.auth.oauth2.BearerToken;
-import com.google.api.client.auth.oauth2.ClientParametersAuthentication;
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.auth.oauth2.StoredCredential;
-import com.google.api.client.auth.oauth2.TokenResponse;
 import com.google.api.client.auth.oauth2.TokenResponseException;
-
 import com.google.api.client.extensions.servlet.auth.oauth2.AbstractAuthorizationCodeCallbackServlet;
-import com.google.api.client.extensions.servlet.auth.oauth2.AbstractAuthorizationCodeServlet;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeRequestUrl;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.auth.oauth2.GoogleOAuthConstants;
-import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
 import com.google.api.client.http.BasicAuthentication;
 import com.google.api.client.http.GenericUrl;
 import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpRequestFactory;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.util.store.FileDataStoreFactory;
-
-import br.com.xti.ouvidoria.controller.MensagemFaceUtil;
-import br.com.xti.ouvidoria.dao.UsuarioDAO;
-import br.com.xti.ouvidoria.model.TbUsuario;
-import br.com.xti.ouvidoria.util.JSFUtils;
-import br.com.xti.ouvidoria.util.PasswordUtils;
-
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+
+import br.com.xti.ouvidoria.dao.UsuarioDAO;
+import br.com.xti.ouvidoria.model.TbUsuario;
 
 @WebServlet(urlPatterns = { "/oauth2Callback" })
 public class AuthenticationCallback extends AbstractAuthorizationCodeCallbackServlet {
 
+	@EJB
+    private UsuarioDAO usuarioDAO;
+	
 	private static final String CLIENT_ID = "11_4psf5i795ta8sow88gcco4sgkow0kcgcwggs4wog0g84ww8gow";
 	private static final String CLIENT_SECRET = "14nampuvl98g08wwkg0k4cgsws4kwss8c080g00wc04og08cco";
 	private static final String CALLBACK_URI = "/oauth2Callback";
@@ -77,42 +68,41 @@ public class AuthenticationCallback extends AbstractAuthorizationCodeCallbackSer
 		
 		//PasswordUtils.getMD5("abcd123456").toUpperCase();
 		
-		HttpTransport transport = this.objAuthorizationCodeFlow.getTransport();
-		HttpRequestFactory requestFactory = transport.createRequestFactory(credential);
-		
-		GenericUrl url = new GenericUrl(USER_INFO_URL);
-		HttpRequest userInfoRequest = requestFactory.buildGetRequest(url);
-		String userIdentity = userInfoRequest.execute().parseAsString();
-		
-		request.getSession().setAttribute("oauthUserIdentity", userIdentity);
-		
-		String oauthUserIdentity = request.getSession().getAttribute("oauthUserIdentity").toString();
-		
 		try {
+			HttpTransport transport = this.objAuthorizationCodeFlow.getTransport();
+			HttpRequestFactory requestFactory = transport.createRequestFactory(credential);
+			
+			GenericUrl url = new GenericUrl(USER_INFO_URL);
+			HttpRequest userInfoRequest = requestFactory.buildGetRequest(url);
+			String userIdentity = userInfoRequest.execute().parseAsString();
+			
+			request.getSession().setAttribute("oauthUserIdentity", userIdentity);
+			
+			String oauthUserIdentity = request.getSession().getAttribute("oauthUserIdentity").toString();
 			
 			JSONObject json = new JSONObject(oauthUserIdentity);
 			String email = json.get("email").toString();
-			UsuarioDAO objUsuarioDAO = new UsuarioDAO();
-			TbUsuario objTbUsuario = objUsuarioDAO.findByEmail(email);
 			
-			if(objTbUsuario == null) {
+			TbUsuario objTbUsuario = usuarioDAO.findByEmail(email);
+			
+			if(objTbUsuario != null) {
 				request.login(objTbUsuario.getNmUsuario(), objTbUsuario.getNmSenha());
 				response.sendRedirect("/GOG/pages/manifestacao/listarmanifestacoes.xhtml");
 			} else {
-				// Redirecionamento do usuário que ainda não possui cadastro no sistema para que preencha os dados adicionais.
-				// @todo: Quando for via OAuth, sugiro que os dados já carregados estejam como readOnly, para não permitir alterar. 
-				//        Somente na alteração dos dados permitiria a edição desses dados que já estão vindo do Host OAuth2.
-				// @todo: Os campos Login e senha, devem ser ocultos nesse cadastro.
+				request.setAttribute("nmUsuario", json.get("full_name").toString());
+				request.setAttribute("nmLogin", json.get("username").toString());
+				request.setAttribute("eeEmail", email);
 				
-				request.setAttribute("full_name", json.get("username").toString());
-				request.setAttribute("email", email);
-				response.sendRedirect("/GOG/pages/externo/cadastrarManifestante.xhtml");
+				RequestDispatcher dispatcher = request.getRequestDispatcher("/pages/externo/cadastrarManifestante.xhtml");
+				dispatcher.forward(request, response);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		
 	  }
+	
+	
 
 	@Override
 	protected void onError(HttpServletRequest req, HttpServletResponse resp, AuthorizationCodeResponseUrl errorResponse)
